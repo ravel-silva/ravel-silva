406ee477a74fcbb97dcdd9f7d8b6a5644edc0951
Ravel Mesquita <ravels78@gmail.com>
2023-10-24 21:45:16 -0300

    Resolvendo Bugs

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -2,6 +2,7 @@
 {
     partial class Form1
     {
+
         /// <summary>
         ///  Required designer variable.
         /// </summary>
@@ -46,185 +47,238 @@
             button15 = new Button();
             button16 = new Button();
             button17 = new Button();
+            C = new Button();
             SuspendLayout();
             // 
             // button1
             // 
+            button1.AllowDrop = true;
             button1.Location = new Point(12, 216);
             button1.Name = "button1";
             button1.Size = new Size(50, 50);
             button1.TabIndex = 0;
+            button1.TabStop = false;
             button1.Text = "1";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
             // button2
             // 
+            button2.AllowDrop = true;
             button2.Location = new Point(68, 216);
             button2.Name = "button2";
             button2.Size = new Size(50, 50);
             button2.TabIndex = 1;
+            button2.TabStop = false;
             button2.Text = "2";
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
             // button3
             // 
+            button3.AllowDrop = true;
             button3.Location = new Point(124, 216);
             button3.Name = "button3";
             button3.Size = new Size(50, 50);
             button3.TabIndex = 2;
+            button3.TabStop = false;
             button3.Text = "3";
             button3.UseVisualStyleBackColor = true;
             button3.Click += button3_Click;
             // 
             // button4
             // 
+            button4.AllowDrop = true;
             button4.Location = new Point(12, 160);
             button4.Name = "button4";
             button4.Size = new Size(50, 50);
             button4.TabIndex = 3;
+            button4.TabStop = false;
             button4.Text = "4";
             button4.UseVisualStyleBackColor = true;
             button4.Click += button4_Click;
             // 
             // button5
             // 
+            button5.AllowDrop = true;
             button5.Location = new Point(68, 160);
             button5.Name = "button5";
             button5.Size = new Size(50, 50);
             button5.TabIndex = 4;
+            button5.TabStop = false;
             button5.Text = "5";
             button5.UseVisualStyleBackColor = true;
             button5.Click += button5_Click;
             // 
             // button6
             // 
+            button6.AllowDrop = true;
             button6.Location = new Point(124, 160);
             button6.Name = "button6";
             button6.Size = new Size(50, 50);
             button6.TabIndex = 5;
+            button6.TabStop = false;
             button6.Text = "6";
             button6.UseVisualStyleBackColor = true;
             button6.Click += button6_Click;
             // 
             // button7
             // 
+            button7.AllowDrop = true;
             button7.Location = new Point(12, 104);
             button7.Name = "button7";
             button7.Size = new Size(50, 50);
             button7.TabIndex = 6;
+            button7.TabStop = false;
             button7.Text = "7";
             button7.UseVisualStyleBackColor = true;
             button7.Click += button7_Click;
             // 
             // button8
             // 
+            button8.AllowDrop = true;
             button8.Location = new Point(68, 104);
             button8.Name = "button8";
             button8.Size = new Size(50, 50);
             button8.TabIndex = 7;
+            button8.TabStop = false;
             button8.Text = "8";
             button8.UseVisualStyleBackColor = true;
             button8.Click += button8_Click;
             // 
             // button9
             // 
+            button9.AllowDrop = true;
             button9.Location = new Point(124, 104);
             button9.Name = "button9";
             button9.Size = new Size(50, 50);
             button9.TabIndex = 8;
+            button9.TabStop = false;
             button9.Text = "9";
             button9.UseVisualStyleBackColor = true;
             button9.Click += button9_Click;
             // 
             // button10
             // 
+            button10.AllowDrop = true;
             button10.Location = new Point(68, 272);
             button10.Name = "button10";
             button10.Size = new Size(50, 50);
             button10.TabIndex = 9;
+            button10.TabStop = false;
             button10.Text = "0";
             button10.UseVisualStyleBackColor = true;
             button10.Click += button10_Click;
             // 
             // Visor
             // 
+            Visor.BackColor = Color.FromArgb(224, 224, 224);
+            Visor.Cursor = Cursors.IBeam;
+            Visor.ImeMode = ImeMode.Off;
             Visor.Location = new Point(12, 12);
             Visor.Multiline = true;
             Visor.Name = "Visor";
+            Visor.ReadOnly = true;
             Visor.Size = new Size(218, 34);
-            Visor.TabIndex = 10;
+            Visor.TabIndex = 0;
+            Visor.TabStop = false;
+            Visor.TextAlign = HorizontalAlignment.Right;
             // 
             // button11
             // 
+            button11.AllowDrop = true;
             button11.Location = new Point(180, 272);
             button11.Name = "button11";
             button11.Size = new Size(50, 50);
             button11.TabIndex = 11;
+            button11.TabStop = false;
             button11.Text = "+";
             button11.UseVisualStyleBackColor = true;
             button11.Click += button11_Click;
             // 
             // button12
             // 
+            button12.AllowDrop = true;
             button12.Location = new Point(124, 272);
             button12.Name = "button12";
             button12.Size = new Size(50, 50);
             button12.TabIndex = 12;
+            button12.TabStop = false;
             button12.Text = "=";
             button12.UseVisualStyleBackColor = true;
             button12.Click += button12_Click;
             // 
             // button13
             // 
+            button13.AllowDrop = true;
             button13.Location = new Point(180, 216);
             button13.Name = "button13";
             button13.Size = new Size(50, 50);
             button13.TabIndex = 13;
+            button13.TabStop = false;
             button13.Text = "-";
             button13.UseVisualStyleBackColor = true;
             button13.Click += button13_Click;
             // 
             // button14
             // 
+            button14.AllowDrop = true;
             button14.Location = new Point(180, 160);
             button14.Name = "button14";
             button14.Size = new Size(50, 50);
             button14.TabIndex = 14;
+            button14.TabStop = false;
             button14.Text = "/";
             button14.UseVisualStyleBackColor = true;
             button14.Click += button14_Click;
             // 
             // button15
             // 
+            button15.AllowDrop = true;
             button15.Location = new Point(180, 104);
             button15.Name = "button15";
             button15.Size = new Size(50, 50);
             button15.TabIndex = 15;
+            button15.TabStop = false;
             button15.Text = "X";
             button15.UseVisualStyleBackColor = true;
             button15.Click += button15_Click;
             // 
             // button16
             // 
-            button16.Location = new Point(124, 52);
+            button16.AllowDrop = true;
+            button16.Location = new Point(180, 52);
             button16.Name = "button16";
-            button16.Size = new Size(106, 46);
+            button16.Size = new Size(50, 46);
             button16.TabIndex = 16;
+            button16.TabStop = false;
             button16.Text = "CE";
             button16.UseVisualStyleBackColor = true;
             button16.Click += button16_Click;
             // 
             // button17
             // 
+            button17.AllowDrop = true;
             button17.Location = new Point(12, 272);
             button17.Name = "button17";
             button17.Size = new Size(50, 50);
             button17.TabIndex = 17;
+            button17.TabStop = false;
             button17.Text = ",";
             button17.UseVisualStyleBackColor = true;
             button17.Click += button17_Click;
+            // 
+            // C
+            // 
+            C.AllowDrop = true;
+            C.Location = new Point(124, 52);
+            C.Name = "C";
+            C.Size = new Size(50, 46);
+            C.TabIndex = 16;
+            C.TabStop = false;
+            C.Text = "C";
+            C.UseVisualStyleBackColor = true;
+            C.Click += C_Click;
             // 
             // Form1
             // 
@@ -232,6 +286,7 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(243, 383);
             Controls.Add(button17);
+            Controls.Add(C);
             Controls.Add(button16);
             Controls.Add(button15);
             Controls.Add(button14);
@@ -275,5 +330,6 @@
         private Button button15;
         private Button button16;
         private Button button17;
+        private Button C;
     }
 }
--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -2,6 +2,7 @@ using Calculator.Operações;
 using System.Text.RegularExpressions;
 using System.Globalization;
 
+
 namespace Calculator
 {
     public partial class Form1 : Form
@@ -9,75 +10,198 @@ namespace Calculator
         public Form1()
         {
             InitializeComponent();
+
+            this.KeyPreview = true; // Habilita a captura de eventos de teclado
+            this.KeyDown += new KeyEventHandler(Form1_BotoesTeclado); // Associa o evento KeyDown
         }
+
+
+
+
+        private void Form1_BotoesTeclado(object sender, KeyEventArgs e) // bug visor duplicado e dpad e os numeros nao funcinam ao mesmo tempo
+        {
+
+            // Verifica qual tecla foi pressionada e realiza a ação correspondente
+            switch (e.KeyCode)
+            {
+                case Keys.D0:
+                case Keys.NumPad0:
+                    Visor.Text += "0";
+                    break;
+                case Keys.D1:
+                case Keys.NumPad1:
+                    Visor.Text += "1";
+                    break;
+                case Keys.D2:
+                case Keys.NumPad2:
+                    Visor.Text += "2";
+                    break;
+                case Keys.D3:
+                case Keys.NumPad3:
+                    Visor.Text += "3";
+                    break;
+                case Keys.D4:
+                case Keys.NumPad4:
+                    Visor.Text += "4";
+                    break;
+                case Keys.D5:
+                case Keys.NumPad5:
+                    Visor.Text += "5";
+                    break;
+                case Keys.D6:
+                case Keys.NumPad6:
+                    Visor.Text += "6";
+                    break;
+                case Keys.D7:
+                case Keys.NumPad7:
+                    Visor.Text += "7";
+                    break;
+                case Keys.D8:
+                case Keys.NumPad8:
+                    Visor.Text += "8";
+                    break;
+                case Keys.D9:
+                case Keys.NumPad9:
+                    Visor.Text += "9";
+                    break;
+                case Keys.Decimal:
+                    Visor.Text += ".";
+                    break;
+                case Keys.Add:
+                    button11_Click(sender, e); // Pressionar o botão de soma
+                    break;
+                case Keys.Subtract:
+                    button13_Click(sender, e); // Pressionar o botão de subtração
+                    break;
+                case Keys.Multiply:
+                    button15_Click(sender, e); // Pressionar o botão de multiplicação
+                    break;
+                case Keys.Divide:
+                    button14_Click(sender, e); // Pressionar o botão de divisão
+                    break;
+                case Keys.Enter:
+                    button12_Click(sender, e); // Pressionar o botão de igual
+                    break;
+                case Keys.Delete:
+                    button16_Click(sender, e);
+                    break;
+                case Keys.Back:
+                        C_Click(sender, e);
+                    break;
+                default:
+                    // Lidar com outras teclas ou ignorá-las
+                    break;
+            }
+
+        }
+
+
+        public void ControlSetFocus(Control control)// remove o foco do botao e direciona para o visor
+        {
+            control?.Focus();
+            Visor.DeselectAll();
+        }
+
 
         Soma Soma = new Soma();
         private decimal primeiroNumero = 0m;
         private string Operador = "";
 
 
-
-
         private void button1_Click(object sender, EventArgs e)
         {
             Visor.Text += "1";
+            ControlSetFocus(Visor);
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             Visor.Text += "2";
+            ControlSetFocus(Visor);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             Visor.Text += "3";
+            ControlSetFocus(Visor);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             Visor.Text += "4";
+            ControlSetFocus(Visor);
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
             Visor.Text += "5";
+            ControlSetFocus(Visor);
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
             Visor.Text += "6";
+            ControlSetFocus(Visor);
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
             Visor.Text += "7";
+            ControlSetFocus(Visor);
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
             Visor.Text += "8";
+            ControlSetFocus(Visor);
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
             Visor.Text += "9";
+            ControlSetFocus(Visor);
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
-            Visor.Text += "0";
+            if (Visor.Text.Contains("0."))
+            {
+                Visor.Text += "0";
+            }
+            else
+            {
+                Visor.Text = "0";
+            }
+
+
+
+
+            //Visor.Text += "0";
+            ControlSetFocus(Visor);
         }
 
-        /* private void IgnZeros(string texto)
-         {
-             // Use a cultura Invariante para garantir que o ponto seja um separador decimal padrão
-             if (decimal.TryParse(texto, NumberStyles.Float, CultureInfo.InvariantCulture, out decimal numero))
-             {
-                 // Use formatação para mostrar a saída
-                  Visor.Text = numero.ToString(numero == (int)numero ? "0" : "0.0#");
-             }
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (Visor.Text != "")
+            {
+                Visor.Text += ".";
+                string cleanedInput = Regex.Replace(Visor.Text, @"\.{2,}", ".");
+                //Relex Substituir todas as ocorrências de um padrão de expressão regular por um valor específico em uma string.
+                // O padrão \.{2,} significa "dois ou mais pontos".
+                //O segundo argumento, ".,", é o texto de substituição e, neste caso, substitui todas as ocorrências encontradas pelo caractere ponto único ".".
+                //Basicamente, ele está substituindo múltiplos pontos consecutivos por um único ponto.
+                Visor.Text = cleanedInput;
+            }
+            else
+            {
+                Visor.Text += "0.";
+                string cleanedInput = Regex.Replace(Visor.Text, @"\.{2,}", ".");
+                Visor.Text = cleanedInput;
+            }
+            ControlSetFocus(Visor);
+        }
 
-         }*/
 
         private void button11_Click(object sender, EventArgs e)//soma
         {
@@ -103,11 +227,12 @@ namespace Calculator
                 {
                     // Use a classe Soma para fazer a soma
                     decimal resultado = Soma.soma(primeiroNumero, segundoNumero);
-                    Visor.Text = resultado.ToString();
+                    Visor.Text = resultado.ToString("0.##", CultureInfo.InvariantCulture);
                     primeiroNumero = 0; // Reinicie o primeiro número para futuras operações
                 }
 
             }
+            ControlSetFocus(Visor);
 
         }
 
@@ -130,11 +255,12 @@ namespace Calculator
                 {
                     // Use a classe Subt para fazer a subtração
                     decimal resultado = Subt.subt(primeiroNumero, segundoNumero);
-                    Visor.Text = resultado.ToString();
+                    Visor.Text = resultado.ToString("0.##", CultureInfo.InvariantCulture);
                     primeiroNumero = 0; // Reinicie o primeiro número para futuras operações
                 }
 
             }
+            ControlSetFocus(Visor);
         }
 
         private void button14_Click(object sender, EventArgs e) // divisao
@@ -156,11 +282,12 @@ namespace Calculator
                 {
                     // Use a classe Divisao para fazer a subtração
                     decimal resultado = Divisao.devisao(primeiroNumero, segundoNumero);
-                    Visor.Text = resultado.ToString();
+                    Visor.Text = resultado.ToString("0.##", CultureInfo.InvariantCulture);
                     primeiroNumero = 0; // Reinicie o primeiro número para futuras operações
                 }
 
             }
+            ControlSetFocus(Visor);
         }
 
         private void button15_Click(object sender, EventArgs e)//multiplicaçao
@@ -182,16 +309,18 @@ namespace Calculator
                 {
                     // Use a classe Muktio para fazer a subtração
                     decimal resultado = Multip.multip(primeiroNumero, segundoNumero);
-                    Visor.Text = resultado.ToString();
+                    Visor.Text = resultado.ToString("0.##", CultureInfo.InvariantCulture);
                     primeiroNumero = 0; // Reinicie o primeiro número para futuras operações
                 }
 
             }
+            ControlSetFocus(Visor);
         }
 
         private void button16_Click(object sender, EventArgs e)//Apagar
         {
             Visor.Text = string.Empty;
+            ControlSetFocus(Visor);
         }
 
         private void button12_Click(object sender, EventArgs e)//igual
@@ -222,16 +351,17 @@ namespace Calculator
                         break;
                 }
 
-                Visor.Text = resultado.ToString();
+                Visor.Text = resultado.ToString("0.##", CultureInfo.InvariantCulture);
             }
-
+            ControlSetFocus(Visor);
         }
 
-        private void button17_Click(object sender, EventArgs e)
+        private void C_Click(object sender, EventArgs e)
         {
-            Visor.Text += ".";
-            string cleanedInput = Regex.Replace(Visor.Text, @"\.{2,}", ".");
-            Visor.Text = cleanedInput;
+            if (Visor.Text.Length > 0)
+            {
+                Visor.Text = Visor.Text.Substring(0, Visor.Text.Length - 1);
+            }
         }
     }
 }
